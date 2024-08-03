187f8df70c40fa15508c50270b8665dde779b3c0
Karolbarros <116102454+Karolbarros@users.noreply.github.com>
2024-08-03 20:08:39 +0000

    mensagens no sistema de login

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
 from flask import Flask, render_template, flash, redirect, request, url_for
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Karol1004"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cadastro.html')
-
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    email = request.form['email']
+    senha = request.form['senha']
+    if email == 'karol' and senha == 'senha123':
+        flash('Login feito com sucesso, você está logado','sucess')
+        return render_template('/index.html')
+    else:
+        flash('Dados incorretos. Email ou senha inválidos', 'danger')
+        return redirect(url_for('login'))
+    
+
+
 @app.route('/doacoes')
 def doacoes():
-    return render_template('doacoes.html')
+     return render_template('doacoes.html')
 
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
+
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+
+
+
 
 
 
